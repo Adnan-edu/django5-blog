7799e59b932b4282adf75d2b742cfa85b9a3354c
Adnan-edu <md.ashikadnan@gmail.com>
2025-02-17 21:17:21 +1100

    SEO-friendly URLs for the blog posts

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.conf import settings
+from django.urls import reverse
 
 # Custom manager to retrieve all posts that have a PUBLISHED status
 class PublishedManager(models.Manager):
@@ -16,7 +17,9 @@ class Post(models.Model):
         PUBLISHED = 'PB', 'Published'
 
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250)
+    slug = models.SlugField(
+        max_length=250,
+        unique_for_date='publish')
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
@@ -43,4 +46,15 @@ class Post(models.Model):
         indexes = [
             models.Index(fields=['-publish']) # Index in descending order
         ]
+
+    def get_absolute_url(self):
+        return reverse(         # Build URL dynamically
+            'blog:post_detail',
+            args=[
+                self.publish.year,
+                self.publish.month,
+                self.publish.day,
+                self.slug
+                ]
+        )
 
