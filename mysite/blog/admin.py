8be1a999c8d2ab165836fcb20b6d94474284b371
Adnan-edu <md.ashikadnan@gmail.com>
2025-02-18 21:59:52 +1100

    Recommending posts by email & Created a comment system.

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post
+from .models import Comment, Post
 
 # Register your models here.
 # Model is registered using custom class
@@ -12,4 +12,10 @@ class PostAdmin(admin.ModelAdmin):
     raw_id_fields = ['author'] # Displayed with a lookup widget
     date_hierarchy = 'publish'
     ordering = ['status', 'publish']
-    show_facets = admin.ShowFacets.ALWAYS # Include total facet counts    
+    show_facets = admin.ShowFacets.ALWAYS # Include total facet counts
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['name', 'email', 'post', 'created', 'active']
+    list_filter = ['active', 'created', 'updated']
+    search_fields = ['name', 'email', 'body']    
